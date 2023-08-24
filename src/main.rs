3eb8878d06497bd50560e5406a933aeb277e82f8
Arthur Pin <arthur_pin@usp.br>
2023-08-24 18:00:06 -0300

    feat: Add argument-based fuzzy filtering

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 use freedesktop_desktop_entry as fde;
+use fuzzy_filter;
 use json;
-use std::fs;
+use std::{env, fs};
 
 fn main() {
-    let mut app_list = json::JsonValue::new_array();
+    let mut json_app_list = json::JsonValue::new_array();
     for path in fde::Iter::new(fde::default_paths()) {
         if let Ok(bytes) = fs::read_to_string(&path) {
             if let Ok(entry) = fde::DesktopEntry::decode(&path, &bytes) {
                 if let Some(name) = entry.name(None) {
                     if let Some(exec) = entry.exec() {
-                        let app = json::object! {
-                            name: name.to_string(),
-                            exec: exec.to_string()
-                        };
-                        let _ = app_list.push(app);
+                        let app = json::object! {name: name.to_string(), exec: exec.to_string()};
+                        let _ = json_app_list.push(app);
                     }
                 }
             }
         }
     }
-    println!("{}", app_list);
+    let mut args = env::args();
+    args.next(); // skip program name
+
+    if let Some(filter) = args.next() {
+        let filter_lower = filter.to_lowercase();
+        let filtered_apps_iter = json_app_list.members().filter_map(|a| {
+            let name_lower = a["name"].to_string().to_lowercase();
+            if fuzzy_filter::matches(&filter_lower, name_lower.as_str()) {
+                Some(a.to_owned())
+            } else {
+                None
+            }
+        });
+        let mut json_app_list = json::JsonValue::new_array();
+        filtered_apps_iter.for_each(|a| json_app_list.push(a.to_owned()).unwrap());
+
+        println!("{}", json_app_list);
+    } else {
+        println!("{}", json_app_list);
+    }
 }
